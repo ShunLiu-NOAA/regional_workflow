08cd21959917f2198cf3cb754023b5f06a4727f0
Shun Liu <Shun.Liu@noaa.gov>
2021-12-13 16:40:00 +0000

    update fit-to-obs files

--- scripts/exregional_run_analysis.sh
+++ scripts/exregional_run_analysis.sh
@@ -1010,7 +1010,7 @@ mv fort.205 fit_pw1
 mv fort.207 fit_rad1
 mv fort.209 fit_rw1
 
-cat fit_p1 fit_w1 fit_t1 fit_q1 fit_pw1 fit_rad1 fit_rw1 > $COMOUT/rrfs.${CYCrun}z.fits.${tmmark}
+cat fit_p1 fit_w1 fit_t1 fit_q1 fit_pw1 fit_rad1 fit_rw1 > $COMOUT/rrfs.t${CYCrun}z.fits.${tmmark}
 cat fort.208 fort.210 fort.211 fort.212 fort.213 fort.220 > $COMOUT/rrfs.t${CYCrun}z.fits2.${tmmark}
 echo Shun debug2
 
